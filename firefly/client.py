4423f870cd0ed7fe14bf2051ae8dd7c1aaf19d5d
Anton Krivenko <anton@krivenko.dev>
2024-11-14 23:15:36 +0000

    Migrated to Firefly III python client

--- firefly/client.py
+++ firefly/client.py
@@ -1,49 +1,51 @@
 import uuid
 from logging import Logger
-from typing import List, Mapping
+from typing import List
 
-import requests
+from firefly_iii_client import ApiClient, configuration, CurrenciesApi, CurrencyStore
 
 from firefly.scheme import FfCurrency
 
 
 class FireflyClient:
-    """Firefly III Client."""
+    """Firefly III Client wrapper."""
 
     __logger: Logger
-    __url: str
-    __token: str
+    __client: ApiClient
 
     def __init__(self, logger: Logger, url: str, token: str) -> None:
         self.__logger = logger
-        self.__url = url
-        self.__token = token
+        self.__client = ApiClient(
+            configuration.Configuration(
+                host=url,
+                access_token=token
+            ),
+            header_name='Content-Type',
+            header_value='application/json'
+        )
 
     def get_currencies(self) -> List[FfCurrency]:
         """Get currencies on the server."""
 
+        api = CurrenciesApi(self.__client)
         page = 1
         currencies = []
 
         while True:
-            params = {'limit': 100, 'page': page}
+            r = api.list_currency(x_trace_id=str(uuid.uuid4()), limit=100, page=page)
 
-            r = requests.get(self.__url + '/v1/currencies', headers=self.__headers(), params=params)
-            r.raise_for_status()
-            j = r.json()
-
-            for currency in j['data']:
+            for currency in r.data:
                 currencies.append(
                     FfCurrency(
-                        id=int(currency['id']),
-                        name=currency['attributes']['name'],
-                        code=currency['attributes']['code'],
-                        symbol=currency['attributes']['symbol'],
-                        enabled=currency['attributes']['enabled'],
+                        id=int(currency.id),
+                        name=currency.attributes.name,
+                        code=currency.attributes.code,
+                        symbol=currency.attributes.symbol,
+                        enabled=currency.attributes.enabled,
                     )
                 )
 
-            if j['meta']['pagination']['current_page'] == j['meta']['pagination']['total_pages']:
+            if r.meta.pagination.current_page == r.meta.pagination.total_pages:
                 break
             page += 1
 
@@ -52,22 +54,17 @@ class FireflyClient:
     def create_currency(self, currency: FfCurrency) -> int:
         """Create a new currency."""
 
-        payload = {"code": currency.code, "name": currency.name, "symbol": currency.symbol}
-        r = requests.post(self.__url + f'/v1/currencies', headers=self.__headers(), json=payload)
-        r.raise_for_status()
-        j = r.json()
-        return int(j['data']['id'])
+        r = CurrenciesApi(self.__client).store_currency(
+            CurrencyStore(
+                code=currency.code,
+                name=currency.name,
+                symbol=currency.symbol,
+            ),
+            x_trace_id=str(uuid.uuid4())
+        )
+        return int(r.data.id)
 
     def enable_currency(self, code: str) -> None:
         """Enable given currency."""
 
-        r = requests.post(self.__url + f'/v1/currencies/{code}/enable', headers=self.__headers())
-        r.raise_for_status()
-
-    def __headers(self) -> Mapping[str, str]:
-        return {
-            'Authorization': f'Bearer {self.__token}',
-            'Content-Type': 'application/json',
-            'Accept': 'application/vnd.api+json',
-            'X-Trace-Id': str(uuid.uuid4()),
-        }
+        CurrenciesApi(self.__client).enable_currency(code)
