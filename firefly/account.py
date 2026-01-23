c6bd81710cb5d025a03bd9abf1efc1af46538f98
Anton Krivenko <anton@krivenko.dev>
2026-01-23 17:14:28 +0000

    Fixed minor issue

--- firefly/account.py
+++ firefly/account.py
@@ -92,7 +92,7 @@ class AccountExporter:
         ff = AccountStore(name=account.name, type=account_type, currency_code=account.currency.name)
         ff.active = config.active
         ff.include_net_worth = config.active
-        if config.opening_balance_date is not None:
+        if config.opening_balance_date:
             ff.opening_balance_date = to_datetime(config.opening_balance_date, '00:00')
             ff.opening_balance = config.opening_balance
 
